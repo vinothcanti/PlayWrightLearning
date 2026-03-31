cdd36f1152bf71f35c3750e0f242b98b684eea3d
vinothraj <vinothcanti@gmail.com>
2026-03-31 20:09:18 +0530

    Initial Playwright test setup

---
+++ tests/register.spec.ts
@@ -0,0 +1,44 @@
+import { test, expect } from '@playwright/test';
+
+// Add this to maximize window
+test.use({
+  launchOptions: {
+    args: ['--start-maximized']
+  }
+});
+
+test('Verify My Account and Register button', async ({ page }) => {
+  await page.goto(
+    'https://tutorialsninja.com/demo/index.php?route=account/register',
+    { timeout: 60000, waitUntil: 'domcontentloaded' }
+    
+  );
+
+  // Click header My Account only
+  await page.locator('header, nav').first()
+    .getByRole('link', { name: /My Account/i })
+    .click();
+
+  // Verify Register visible
+  await expect(
+    page.getByRole('link', { name: 'Register' }).first()
+  ).toBeVisible();
+
+   // Click Login from dropdown
+  await page.locator('#top-links')
+    .getByRole('link', { name: 'Login' })
+    .click();
+
+  // Enter login details
+  await page.getByLabel('E-Mail Address').fill('vinoth123test@gmail.com');
+  await page.getByLabel('Password').fill('Vinoth@3532');
+
+  // Click Login button
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  // Verify login success
+  await expect(
+    page.getByRole('heading', { name: 'My Account' })
+  ).toBeVisible();
+
+});
